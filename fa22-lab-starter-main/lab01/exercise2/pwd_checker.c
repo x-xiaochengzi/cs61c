8faed65b0134405a3d88f4f14789d4b8356ea7e1
x-xiaochengzi <230616964@qq.com>
2023-08-11 16:54:29 +0800

    done with lab01

--- fa22-lab-starter-main/lab01/exercise2/pwd_checker.c
+++ fa22-lab-starter-main/lab01/exercise2/pwd_checker.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include "pwd_checker.h"
+#include <assert.h>
 
 /*
 Password checker
@@ -19,23 +20,28 @@ For the simplicity of this exercise:
 */
 
 /* Returns true if the length of PASSWORD is at least 10, false otherwise */
-bool check_length(const char *password) {
+bool check_length(const char *password)
+{
     int length = strlen(password);
-    bool meets_len_req = (length <= 10);
+    bool meets_len_req = (length >= 10);
     return meets_len_req;
 }
 
 /* Returns true if LETTER is in the range [LOWER, UPPER], false otherwise */
-bool check_range(char letter, char lower, char upper) {
-    bool is_in_range = (letter > lower && letter < upper);
+bool check_range(char letter, char lower, char upper)
+{
+    bool is_in_range = (letter >= lower && letter <= upper);
     return is_in_range;
 }
 
 /* Returns true if PASSWORD contains at least one upper case letter, false otherwise */
-bool check_upper(const char *password) {
-    while (password != '\0') {
+bool check_upper(const char *password)
+{
+    while (*password != '\0')
+    {
         bool is_in_range = check_range(*password, 'A', 'Z');
-        if (is_in_range) {
+        if (is_in_range)
+        {
             return true;
         }
         ++password;
@@ -44,10 +50,13 @@ bool check_upper(const char *password) {
 }
 
 /* Returns true if PASSWORD contains at least one lower case letter, false otherwise */
-bool check_lower(const char *password) {
-    while (*password != '\0') {
+bool check_lower(const char *password)
+{
+    while (*password != '\0')
+    {
         bool is_in_range = check_range(*password, 'a', 'z');
-        if (is_in_range) {
+        if (is_in_range)
+        {
             return true;
         }
         ++password;
@@ -56,9 +65,12 @@ bool check_lower(const char *password) {
 }
 
 /* Returns true if PASSWORD contains at least one number, false otherwise */
-bool check_number(const char *password) {
-    while (password != '\0') {
-        if (check_range(password, 0, 9)) {
+bool check_number(const char *password)
+{
+    while (*password != '\0')
+    {
+        if (check_range(*password, '0', '9'))
+        {
             return true;
         }
         ++password;
@@ -67,18 +79,20 @@ bool check_number(const char *password) {
 }
 
 /* Returns true if the person's first and last name are NOT in the password, false otherwise */
-bool check_name(const char *first_name, const char *last_name, const char *password) {
+bool check_name(const char *first_name, const char *last_name, const char *password)
+{
     /* Type "man strstr" in your terminal to learn what strstr does!
         To exit the man pages, press 'q' */
     /* Hint: a NULL pointer will evaluate to False in a logical statement while a non-NULL pointer
         will evaluate to True */
-    const char *first = strstr(*password, first_name);
+    const char *first = strstr(password, first_name);
     const char *last = strstr(password, last_name);
     return (!first && !last);
 }
 
 /* Returns true if PASSWORD meets the conditions specified above */
-bool check_password(const char *first_name, const char *last_name, const char *password) {
+bool check_password(const char *first_name, const char *last_name, const char *password)
+{
     bool length, upper, lower, number, name;
     lower = check_lower(password);
     length = check_length(password);
--- fa22-lab-starter-main/lab01/exercise3/ll_cycle.c
+++ fa22-lab-starter-main/lab01/exercise3/ll_cycle.c
@@ -1,6 +1,20 @@
 #include <stddef.h>
 #include "ll_cycle.h"
 
-int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+int ll_has_cycle(node *head)
+{
+    node *fast = head;
+    node *slow = head;
+
+    while (fast && fast->next)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+
+        if (fast == slow)
+        {
+            return 1;
+        }
+    }
+    return 0;
 }
