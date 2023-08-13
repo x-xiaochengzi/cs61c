395dfd6dcdc00f8c9aabef34c9d25574633da5a3
x-xiaochengzi <230616964@qq.com>
2023-08-13 12:28:24 +0800

    done with lab02

--- fa22-lab-starter-main/lab02/bit_ops.c
+++ fa22-lab-starter-main/lab02/bit_ops.c
@@ -2,18 +2,25 @@
 #include "bit_ops.h"
 
 /* Returns the Nth bit of X. Assumes 0 <= N <= 31. */
-unsigned get_bit(unsigned x, unsigned n) {
-    /* YOUR CODE HERE */
-    return -1; /* UPDATE WITH THE CORRECT RETURN VALUE*/
+unsigned get_bit(unsigned x, unsigned n)
+{
+    return ((x & (1 << n)) >> n);
 }
 
 /* Set the nth bit of the value of x to v. Assumes 0 <= N <= 31, and V is 0 or 1 */
-void set_bit(unsigned *x, unsigned n, unsigned v) {
-    /* YOUR CODE HERE */
+void set_bit(unsigned *x, unsigned n, unsigned v)
+{
+    unsigned mask = (v << n);
+
+    // clear nth bit
+    *x &= ~(1 << n);
+
+    // set nth bit
+    *x |= mask;
 }
 
 /* Flips the Nth bit in X. Assumes 0 <= N <= 31.*/
-void flip_bit(unsigned *x, unsigned n) {
-    /* YOUR CODE HERE */
+void flip_bit(unsigned *x, unsigned n)
+{
+    *x ^= (1 << n);
 }
-
